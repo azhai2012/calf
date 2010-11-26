05a620a61821038ef7c589806d369fcc49db3891
azhai <azhai2012@gmail.com>
2010-11-26 18:25:23 +0800

    20111126-1

--- application/classes/controller/welcome.php
+++ application/classes/controller/welcome.php
@@ -2,9 +2,59 @@
 
 class Controller_Welcome extends Controller {
 
-	public function action_index()
-	{
-		$this->request->response = 'hello, world! azhai';
+	private $pub;
+	private  $template='';
+	private $model;
+
+	public function before(){
+		$this->template= View::factory('welcome');
+		$this->model= new Model_Menus();
+		$this->admin= new Model_Admin();
+		
 	}
+	
+	public function action_index()
+	{	
+		parent::before();
+        $sk = array_key_exists('sk',$_GET)?$_GET['sk']:'';
+		$u = new Calf_Menus();
+		$result = Calfpub::getmenus($u);
+		$head=''; 
+	    foreach ($result as $key => $value){
+	    	$head.= $key.":".$value->name;
+	    } 
+	    $this->template->head=$head;
+	    $this->template->rfloat=array(array('name'=>'管理','url'=>'/?sk=admin'),array('name'=>'首页','url'=>'/'));
+	    $this->template->contentcol=''; 
+	    
+		if (substr($sk,0,2)==='ad')
+		{
+	        $this->template->menus='<script>Azhai.onPages({"type":"","id":"navside","content":\''.$this->model->get_admin_menus().'\'});</script>';
+			if ($sk==='adr') 
+			{
+		      //$this->template->contentcol ='<script>Azhai.onPages({"type":"","id":"contentcol","content":\''.$this->admin->get_admin_roles().'\'});</script>';
+			   $this->template->contentcol ='<script>Azhai.onPages({"type":"ajax","ajax":"/ajax?sk=adr","id":"contentcol","loadingid":"loadingIndicator"});</script>';
+		 
+			}
+		}
+	 	else
+	 	{
+	    	$this->template->menus='<script>Azhai.onPages({"type":"","id":"navside","content":\''.$this->model->get_menus().'\'});</script>';
+	    
+		}
+		
+	    $this->template->css='<script>Azhai.onPages({"type":"css","css":["/media/css/basic.css?'.time().'"]});</script>';
+		
+		
+		
+		
+		
+	}
+	
+	public function after(){
+		$this->request->response = $this->template;
+	}
+	
+	
 
 } // End Welcome
