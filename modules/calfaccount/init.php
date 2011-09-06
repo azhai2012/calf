7a9f746ae3c56fd49473ab9a8b97f5628b6f2277
root <root@azhai2011-Inspiron-N5010>
2011-09-07 01:01:02 +0800

    2011-09-07 1:00  Delete not use old module

--- modules/calfaccount/init.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-Route::set('account', '(<controller>(/<action>(/<id>)))')
-	->defaults(array(
-		'controller' => 'account',
-		'action'     => 'index',
-	));
--- modules/calfsupplier/classes/calfsupplier.php
+++
@@ -1,4 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class CalfSupplier extends Kohana_Supplier {}
-
