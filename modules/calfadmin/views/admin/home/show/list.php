c4e95bfa565a8dc165017a806b7d22e53d729478
azhai2012 <azhai2012@gmail.com>
2011-10-18 15:20:56 +0800

    2011-10-18 15:20

--- modules/calfadmin/views/admin/home/show/list.php
+++ modules/calfadmin/views/admin/home/show/list.php
@@ -23,30 +23,25 @@
       <th>生效日期</th>
       <th>状态</th>
   </tr>
+   <?php $array_data = array(
+	   array('id' =>1123,'title'=>'新品区','sql_txt'=>'select * from tablename','create_date'=>'2011-01-01','begin_date'=>'2011-03-01','active'=>1),
+	   array('id' =>1123,'title'=>'保健品区','sql_txt'=>'select * from tablename','create_date'=>'2011-01-01','begin_date'=>'2011-03-01','active'=>1),
+	   array('id' =>1123,'title'=>'器械区','sql_txt'=>'select * from tablename','create_date'=>'2011-01-01','begin_date'=>'2011-03-01','active'=>1),
+	   array('id' =>1123,'title'=>'新品区','sql_txt'=>'select * from tablename','create_date'=>'2011-01-01','begin_date'=>'2011-03-01','active'=>1),
+	   array('id' =>1123,'title'=>'新品区','sql_txt'=>'select * from tablename','create_date'=>'2011-01-01','begin_date'=>'2011-03-01','active'=>1),
+	   array('id' =>1123,'title'=>'新品区','sql_txt'=>'select * from tablename','create_date'=>'2011-01-01','begin_date'=>'2011-03-01','active'=>1),
+	); ?>
+   <?php foreach ($array_data as $key => $value): ?>
    <tr>
-    <td>112</td>
-    <td>新品区</td>
-    <td><code>select * from tablename</code></td>   
-    <td>2011-01-01</td>
-    <td>2011-01-02</td>
-    <td>激活</td>
+    <td><?php echo $value['id']; ?></td>
+    <td><?php echo $value['title'] ?></td>
+    <td><code><?php echo $value['sql_txt'] ?></code></td>   
+    <td><?php echo $value['create_date'] ?></td>
+    <td><?php echo $value['begin_date'] ?></td>
+    <td><?php echo ($value['active']===1) ? '激活' : '未激活' ; ?></td>
   </tr>
-  <tr>
-    <td>112</td>
-    <td>保健品区</td>
-    <td><code>select * from tablename</code></td>
-    <td>2011-01-01</td>
-    <td>2011-01-02</td>
-    <td>激活</td>
-  </tr>
-  <tr>
-  <td>112</td>
-    <td>器械区</td>
-    <td><code>select * from tablename</code></td>
-    <td>2011-01-01</td>
-    <td>2011-01-02</td>
-    <td>激活</td>
-  </tr>
+  <?php endforeach ?> 
+ 
   </tbody>
     </table>
     </div>     
